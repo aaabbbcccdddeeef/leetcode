c452c57bd541b087b3f25f378d93d9cc92d1976e
liuzhch1 <liuzhch1@shanghaitech.edu.cn>
2022-04-15 17:29:56 +0800

    p438-optimize code

--- Hot-100/p438.java
+++ Hot-100/p438.java
@@ -2,34 +2,37 @@ import java.util.*;
 
 public class p438 {
     public List<Integer> findAnagrams(String s, String p) {
-        int sLen = s.length(), pLen = p.length();
-        if (sLen < pLen) {
+        int sLen=s.length(),pLen=p.length();
+        if(sLen<pLen){
             return new ArrayList<Integer>();
         }
-        int[] count = new int[26];
-        int[] pCount = new int[26];
-        for (int i = 0; i < pLen; i++) {
-            count[s.charAt(i) - 'a'] += 1;
-            pCount[p.charAt(i) - 'a'] += 1;
+        int[] count=new int[26];
+        for(int i=0;i<pLen;i++){
+            ++count[s.charAt(i)-'a'];
+            --count[p.charAt(i)-'a'];
         }
-        int need = 0;
-        for (int i = 0; i < 26; i++) {
-            need += count[i] < pCount[i] ? pCount[i] - count[i] : 0;
+        int need=0;
+        for(int i=0;i<26;i++){
+            need+=count[i]==0?0:1;
         }
-        List<Integer> ans = new ArrayList<Integer>();
-        if (need == 0) {
+        List<Integer> ans=new ArrayList<Integer>();
+        if(need==0){
             ans.add(0);
         }
-        for (int i = 1; i + pLen <= sLen; i++) {
-            if (count[s.charAt(i - 1) - 'a'] <= pCount[s.charAt(i - 1) - 'a']) {
+        for(int i=1;i+pLen<=sLen;i++){
+            count[s.charAt(i-1)-'a']--;
+            if(count[s.charAt(i-1)-'a']==0){
+                need--;
+            }else if(count[s.charAt(i-1)-'a']==-1){
                 need++;
             }
-            count[s.charAt(i - 1) - 'a']--;
-            if (count[s.charAt(i + pLen - 1) - 'a'] < pCount[s.charAt(i + pLen - 1) - 'a']) {
+            count[s.charAt(i+pLen-1)-'a']++;
+            if(count[s.charAt(i+pLen-1)-'a']==0){
                 need--;
+            }else if(count[s.charAt(i+pLen-1)-'a']==1){
+                need++;
             }
-            count[s.charAt(i + pLen - 1) - 'a']++;
-            if (need == 0) {
+            if(need==0){
                 ans.add(i);
             }
         }
