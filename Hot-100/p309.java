cdf9f17a364d642705567f7f54220f7b2d7a6fd4
Liuzhch1 <liuzhch1@shanghaitech.edu.cn>
2022-05-01 15:22:31 +0800

    p309-BestTimeToBuy&SellStockwithCooldown-DP

---
+++ Hot-100/p309.java
@@ -0,0 +1,20 @@
+public class p309 {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        if (n == 0) {
+            return 0;
+        }
+        int f0 = -prices[0];
+        int f1 = 0;
+        int f2 = 0;
+        for (int i = 1; i < n; i++) {
+            int newf0 = Math.max(f0, f2 - prices[i]);
+            int newf1 = f0 + prices[i];
+            int newf2 = Math.max(f1, f2);
+            f0 = newf0;
+            f1 = newf1;
+            f2 = newf2;
+        }
+        return Math.max(f1, f2);
+    }
+}
