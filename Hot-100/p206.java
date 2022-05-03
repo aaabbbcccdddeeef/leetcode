03fbfaa6a5d5ae1a56dab10e3edabd7d8bf7c21d
Liuzhch1 <liuzhch1@shanghaitech.edu.cn>
2022-05-03 10:33:25 +0800

    p206-ReverseLinkedList-iteration

--- Hot-100/p206.java
+++ Hot-100/p206.java
@@ -1,11 +1,12 @@
 public class p206 {
     public ListNode reverseList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
+        ListNode prev = null, cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
         }
-        ListNode newHead = reverseList(head.next);
-        head.next.next = head;
-        head.next = null;
-        return newHead;
+        return prev;
     }
 }
