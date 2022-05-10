8290ea934cc051558af2455c5084238d5956b667
Liuzhch1 <liuzhch1@shanghaitech.edu.cn>
2022-05-10 10:49:54 +0800

    p114-FlattenBinaryTree2LinkedList

---
+++ Hot-100/p114.java
@@ -0,0 +1,18 @@
+public class p114 {
+    public void flatten(TreeNode root) {
+        while (root != null) {
+            if (root.left == null) {
+                root = root.right;
+                continue;
+            }
+            TreeNode left = root.left;
+            while (left.right != null) {
+                left = left.right;
+            }
+            left.right = root.right;
+            root.right = root.left;
+            root.left = null;
+            root = root.right;
+        }
+    }
+}
