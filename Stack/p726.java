4bb9caa92aefab6ab90589b41f2f9d1739f32684
Liuzhch1 <liuzhch1@shanghaitech.edu.cn>
2022-06-26 17:23:04 +0800

    LC-726 add some comments

--- Stack/p726.java
+++ Stack/p726.java
@@ -9,14 +9,17 @@ public class p726 {
     int idx, len;
 
     public String countOfAtoms(String fml) {
-        this.formula = fml;
-        this.idx = 0;
+        this.formula = fml; // the chemical formula
+        this.idx = 0; // the index we are handling
         this.len = formula.length();
+        // create the map stack
         Deque<Map<String, Integer>> stack = new ArrayDeque<>();
+        // default map containing atoms outside any parentheses, eg:`Fe` in `Fe2(SO4)3`
         stack.push(new HashMap<String, Integer>());
         while (idx < len) {
             switch (formula.charAt(idx)) {
                 case '(':
+                    // create a new inner map to hold atoms inside the parenthese
                     stack.push(new HashMap<String, Integer>());
                     idx++;
                     break;
@@ -31,12 +34,15 @@ public class p726 {
                     }
                     break;
                 default:
+                    // atom
+                    // record this atom's number
                     Map<String, Integer> map = stack.peek();
                     String atom = nextAtom();
                     int n = nextNum();
                     map.put(atom, map.getOrDefault(atom, 0) + n);
             }
         }
+        // turn outest map to a treeMap so that the atom name is ordered
         TreeMap<String, Integer> treeMap = new TreeMap<>(stack.peek());
         StringBuilder sb = new StringBuilder();
         for (String s : treeMap.keySet()) {
@@ -51,6 +57,7 @@ public class p726 {
 
     private String nextAtom() {
         int start = idx++;
+        // continue matching while next character is a lowercase character
         while (idx < len && Character.isLowerCase(formula.charAt(idx))) {
             idx++;
         }
@@ -58,10 +65,12 @@ public class p726 {
     }
 
     private int nextNum() {
+        // If the following is not a number, return the default 1
         if (idx == len || !Character.isDigit(formula.charAt(idx))) {
             return 1;
         }
         int start = idx++;
+        // continue matching while next character is a digit
         while (idx < len && Character.isDigit(formula.charAt(idx))) {
             idx++;
         }
