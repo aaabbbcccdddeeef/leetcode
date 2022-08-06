beb23dd14d5284859c97ad998e87796da04dc460
Liuzhch1 <liuzhch1@shanghaitech.edu.cn>
2022-08-06 19:07:13 +0800

    LC1408-String Matching in an Array
    This problem can also be solve by KMP\AC Automaton...
    It's so complex, handle them later...

---
+++ Stack/p1408.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class p1408 {
+    public List<String> stringMatching(String[] words) {
+        List<String> ans = new ArrayList<>();
+        int n = words.length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i != j && words[j].contains(words[i])) {
+                    ans.add(words[i]);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
