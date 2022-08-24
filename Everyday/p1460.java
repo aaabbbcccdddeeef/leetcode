9f83feb2198a69e2d8030865a65b6d2e943cab5d
Liuzhch1 <liuzhch1@shanghaitech.edu.cn>
2022-08-24 20:59:28 +0800

    LC1460. Make Two Arrays Equal by Reversing Sub-arrays

---
+++ Everyday/p1460.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class p1460 {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        Arrays.sort(target);
+        Arrays.sort(arr);
+        return Arrays.equals(target, arr);
+    }
+}
